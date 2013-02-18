f89cd59f3a4ca38a93d31ea2f4c42c3b74817438
Marshall Bowers <elliott.codes@gmail.com>
2013-02-18 15:24:29 -0500

    Program functionality completed

--- Assignment One/src/assignment/FrozenFood.java
+++ Assignment One/src/assignment/FrozenFood.java
@@ -4,53 +4,83 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FrozenFood {
+	// Class variable declaration
 	private Scanner input = new Scanner(System.in);
 	private String name;
 	private String manufacturer;
 	private String nutrients;
-	
-	public FrozenFood(String n, String m, String nu) {
-		name = n;
-		manufacturer = m;
-		nutrients = nu;
-	}
-	
+
+	/**
+	 * Creates a new FrozenFood object with all blank fields.
+	 */
 	public FrozenFood() {
 		name = "";
 		manufacturer = "";
 		nutrients = "";
 	}
 
-	public String getName() {
-		return name;
-	}
-	
-	public String getManufacturer() {
-		return manufacturer;
-	}
-	
-	public String getNutrients() {
-		return nutrients;
-	}
-	
 	/**
-	 * Display the names of all frozen food products
-	 * @param foodList
+	 * Creates a new FrozenFood object whose fields are initialized to the given
+	 * parameters.
+	 * 
+	 * @param n
+	 * @param m
+	 * @param nu
 	 */
-	public void displayFrozenFoodNames(ArrayList<FrozenFood> foodList) {
-		for (FrozenFood i : foodList)
-			System.out.println(i.getName());
+	public FrozenFood(String n, String m, String nu) {
+		name = n;
+		manufacturer = m;
+		nutrients = nu;
 	}
 
 	/**
-	 * Display all data for a specific frozen food product
+	 * Returns the name of the FrozenFood.
+	 * 
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * Returns the manufacturer of the FrozenFood.
+	 * 
+	 * @return manufacturer
+	 */
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	/**
+	 * Returns the nutrients of the FrozenFood.
+	 * 
+	 * @return nutrients
+	 */
+	public String getNutrients() {
+		return nutrients;
+	}
+
+	/**
+	 * Display the names of all FrozenFood products.
+	 * 
+	 * @param foodList
+	 */
+	public void displayFrozenFoodNames(ArrayList<FrozenFood> foodList) {
+		System.out.println("Frozen Food Products: ");
+		for (FrozenFood i : foodList)
+			System.out.println("  " + i.getName());
+	}
+
+	/**
+	 * Display all data for a specific FrozenFood product.
+	 * 
 	 * @param foodList
 	 */
 	public void displaySpecificFrozenFood(ArrayList<FrozenFood> foodList) {
 		System.out.print("Enter the name of a specific frozen food product: ");
 		String name = input.nextLine().toUpperCase();
-		FrozenFood result = binarySearch(foodList, name, 0, foodList.size(), "Name");
-		
+		FrozenFood result = binarySearch(foodList, name, 0, foodList.size());
+
 		if (result != null) {
 			System.out.println("\nName: " + result.getName());
 			System.out.println("Manufacturer: " + result.getManufacturer());
@@ -59,23 +89,25 @@ public class FrozenFood {
 			System.out.println(name + " was not found.");
 		}
 	}
-	
+
 	/**
-	 * Display the names of all frozen food products from a given manufacturer
+	 * Display the names of all FrozenFood products from a given manufacturer.
+	 * 
 	 * @param foodList
 	 */
 	public void displayFrozenFoodWManufacturer(ArrayList<FrozenFood> foodList) {
 		System.out.print("Enter the name of a frozen food manufacturer: ");
 		String manufacturer = input.nextLine().toUpperCase();
 		ArrayList<FrozenFood> results = new ArrayList<FrozenFood>();
-		
+
 		for (FrozenFood f : foodList) {
 			if (f.getManufacturer().equals(manufacturer))
 				results.add(f);
 		}
-		
+
 		if (results.size() > 0) {
-			System.out.println("\n" + manufacturer + "makes these frozen foods: ");
+			System.out.println("\n" + manufacturer
+					+ "makes these frozen foods: ");
 			for (FrozenFood f : results) {
 				System.out.println("  " + f.getName());
 			}
@@ -85,21 +117,23 @@ public class FrozenFood {
 	}
 
 	/**
-	 * Display the names of all products with a given top nutrient
+	 * Display the names of all FrozenFood products with a given top nutrient.
+	 * 
 	 * @param foodList
 	 */
 	public void displayFrozenFoodWNutrient(ArrayList<FrozenFood> foodList) {
 		System.out.print("Enter the name of a frozen food nutrient: ");
 		String nutrient = input.nextLine().toUpperCase();
 		ArrayList<FrozenFood> results = new ArrayList<FrozenFood>();
-		
+
 		for (FrozenFood f : foodList) {
 			if (f.getNutrients().contains(nutrient))
 				results.add(f);
 		}
-		
+
 		if (results.size() > 0) {
-			System.out.println("\nThese frozen foods contain " + nutrient + ": ");
+			System.out.println("\nThese frozen foods contain " + nutrient
+					+ ": ");
 			for (FrozenFood f : results) {
 				System.out.println("  " + f.getName());
 			}
@@ -107,38 +141,30 @@ public class FrozenFood {
 			System.out.println("That nutrient was not found.");
 		}
 	}
-	
-	private FrozenFood binarySearch(ArrayList<FrozenFood> foodList, String key, int min, int max, String type) {
+
+	/**
+	 * Searches the given list with a binary search until the item matching the
+	 * key is found.
+	 * 
+	 * @param foodList
+	 * @param key
+	 * @param min
+	 * @param max
+	 * @return the FrozenFood object being searched for
+	 */
+	private FrozenFood binarySearch(ArrayList<FrozenFood> foodList, String key,
+			int min, int max) {
 		if (max < min)
 			return null;
 		else {
 			int mid = (min + max) / 2;
-			
-			switch(type) {
-			case "Name":
-					if (foodList.get(mid).getName().compareTo(key) > 0)
-						return binarySearch(foodList, key, min, mid - 1, type);
-					else if (foodList.get(mid).getName().compareTo(key) < 0)
-						return binarySearch(foodList, key, mid + 1, max, type);
-					else
-						return foodList.get(mid);
-			case "Manufacturer":
-				if (foodList.get(mid).getManufacturer().compareTo(key) > 0)
-					return binarySearch(foodList, key, min, mid - 1, type);
-				else if (foodList.get(mid).getManufacturer().compareTo(key) < 0)
-					return binarySearch(foodList, key, mid + 1, max, type);
-				else
-					return foodList.get(mid);
-			case "Nutrients":
-				if (foodList.get(mid).getNutrients().compareTo(key) > 0)
-					return binarySearch(foodList, key, min, mid - 1, type);
-				else if (foodList.get(mid).getNutrients().compareTo(key) < 0)
-					return binarySearch(foodList, key, mid + 1, max, type);
-				else
-					return foodList.get(mid);
-			default:
-				return null;
-			}
+
+			if (foodList.get(mid).getName().compareTo(key) > 0)
+				return binarySearch(foodList, key, min, mid - 1);
+			else if (foodList.get(mid).getName().compareTo(key) < 0)
+				return binarySearch(foodList, key, mid + 1, max);
+			else
+				return foodList.get(mid);
 		}
 	}
 }
