cc9a6396e375b873f7a765e18e48b62273b755fa
Marshall Bowers <elliott.codes@gmail.com>
2013-02-24 16:47:39 -0500

    Implemented readIn()

--- 2 - Assignment Two/src/assignment/FrozenFood.java
+++ 2 - Assignment Two/src/assignment/FrozenFood.java
@@ -8,31 +8,71 @@
 
 package assignment;
 
+import java.util.Scanner;
+
 public abstract class FrozenFood extends Element {
 	// Class variable declaration
 	private String name;
 	private String manufacturer;
 	private String nutrients;
-	
+
+	/**
+	 * Creates a new FrozenFood object with all blank fields.
+	 */
+	public FrozenFood() {
+		name = "";
+		manufacturer = "";
+		nutrients = "";
+	}
+
+	/**
+	 * Creates a new FrozenFood object whose fields are initialized to the given
+	 * parameters.
+	 * 
+	 * @param name
+	 * @param manufacturer
+	 * @param nutrients
+	 */
 	public FrozenFood(String name, String manufacturer, String nutrients) {
-		this.name = name;
-		this.manufacturer = manufacturer;
-		this.nutrients = nutrients;
+		this.name = name.toUpperCase();
+		this.manufacturer = manufacturer.toUpperCase();
+		this.nutrients = nutrients.toUpperCase();
 	}
-	
+
 	public void readIn() {
-		// TODO implement readIn()
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter the name of the Frozen Food: ");
+		name = input.nextLine().toUpperCase();
+
+		System.out.print("Enter the name of the manufacturer: ");
+		manufacturer = input.nextLine().toUpperCase();
+
+		System.out.print("Enter the number of nutrients for this food: ");
+		int numNu = Integer.parseInt(input.nextLine());
+
+		System.out.print("Enter the nutrients when prompted: \n");
+		nutrients = "";
+
+		for (int i = 0; i < numNu; i++) {
+			System.out.print((i + 1) + ") ");
+			nutrients += input.nextLine().toUpperCase();
+			if (i != numNu - 1)
+				nutrients += ", ";
+		}
+
+		input.close();
 	}
-	
+
 	public void display() {
 		// TODO implement display()
 	}
-	
+
 	public boolean equals(Element dobj) {
 		// TODO implement equals(Element dobj)
 		return false; // Temporary error fix
 	}
-	
+
 	public Element clone() {
 		// TODO implement clone()
 		return null; // Temporary error fix
