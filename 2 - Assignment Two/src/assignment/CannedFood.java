0b6f1ed6785ff0b051931255b1eb370743801031
Marshall Bowers <elliott.codes@gmail.com>
2013-02-24 15:26:31 -0500

    ElementSet

--- 2 - Assignment Two/src/assignment/CannedFood.java
+++ 2 - Assignment Two/src/assignment/CannedFood.java
@@ -9,5 +9,30 @@
 package assignment;
 
 public abstract class CannedFood extends Element {
+	// Class variable declaration
+	private String name;
+	private String manufacturer;
+	private String nutrients;
+	
+	public CannedFood(String name, String manufacturer, String nutrients) {
+		this.name = name;
+		this.manufacturer = manufacturer;
+		this.nutrients = nutrients;
+	}
+	
+	public void readIn() {
 
+	}
+
+	public void display() {
+
+	}
+
+	public boolean equals(Element dobj) {
+		return false;
+	}
+
+	public Element clone() {
+		return null;
+	}
 }
--- 2 - Assignment Two/src/assignment/FrozenFood.java
+++ 2 - Assignment Two/src/assignment/FrozenFood.java
@@ -9,5 +9,30 @@
 package assignment;
 
 public abstract class FrozenFood extends Element {
+	// Class variable declaration
+	private String name;
+	private String manufacturer;
+	private String nutrients;
 	
+	public FrozenFood(String name, String manufacturer, String nutrients) {
+		this.name = name;
+		this.manufacturer = manufacturer;
+		this.nutrients = nutrients;
+	}
+	
+	public void readIn() {
+		
+	}
+	
+	public void display() {
+		
+	}
+	
+	public boolean equals(Element dobj) {
+		return false;
+	}
+	
+	public Element clone() {
+		return null;	
+	}
 }
