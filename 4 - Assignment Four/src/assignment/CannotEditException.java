6223f09686a05836a8fb3d0ecc25336e15bbf4ae
Marshall <mad.mod.max@gmail.com>
2013-04-11 16:02:59 -0400

    Exceptions written and implemented.

--- 4 - Assignment Four/src/assignment/CannotEditException.java
+++ 4 - Assignment Four/src/assignment/CannotEditException.java
@@ -2,5 +2,7 @@ package assignment;
 
 @SuppressWarnings("serial")
 public class CannotEditException extends Exception {
-
+	public CannotEditException(String error) {
+		System.out.println("\n" + error);
+	}
 }
--- 4 - Assignment Four/src/assignment/DuplicateObjectException.java
+++ 4 - Assignment Four/src/assignment/DuplicateObjectException.java
@@ -2,5 +2,7 @@ package assignment;
 
 @SuppressWarnings("serial")
 public class DuplicateObjectException extends Exception {
-
+	public DuplicateObjectException(String error) {
+		System.out.println("\n" + error);
+	}
 }
