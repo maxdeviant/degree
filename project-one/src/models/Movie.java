e304bff16e85f4656845ce97249a0ec8a7643a74
maxdeviant <mad.mod.max@gmail.com>
2014-02-28 10:20:34 -0500

    Redoing Models.

---
+++ project-one/src/models/Movie.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Movie {
+    private int id;
+    private String title;
+    private int year;
+    private String description;
+
+    public Movie(int id, String title, int year, String description) {
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.description = description;
+    }
+
+    public String toString() {
+        return String.format("ID: %s, Title: %s, Year: %s Description: %s", id, title, year, description);
+    }
+}
