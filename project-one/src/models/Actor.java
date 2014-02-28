a42637ca2e62d95ad416c721d87a7c11a97b4373
maxdeviant <mad.mod.max@gmail.com>
2014-02-28 10:40:54 -0500

    Generic model type.

--- project-one/src/models/Actor.java
+++ project-one/src/models/Actor.java
@@ -1,5 +1,8 @@
 package models;
 
+import java.sql.ResultSet;
+import java.util.HashSet;
+
 public class Actor extends Model {
     private int id;
     private String name;
@@ -9,6 +12,24 @@ public class Actor extends Model {
         this.id = id;
         this.name = name;
         this.birthYear = birthYear;
+    }
+
+    public HashSet<Actor> load(ResultSet results) {
+        HashSet<Actor> set = new HashSet<Actor>();
+
+        try {
+            while (results.next()) {
+                int id = results.getInt("id");
+                String name = results.getString("name");
+                int birthYear = results.getInt("birth_year");
+
+                set.add(new Actor(id, name, birthYear));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return set;
     }
 
     public String toString() {
--- project-one/src/models/Model.java
+++ project-one/src/models/Model.java
@@ -1,5 +1,13 @@
 package models;
 
-public abstract class Model {
+import java.sql.ResultSet;
+import java.util.HashSet;
 
+public abstract class Model<T> {
+    HashSet<T> load(ResultSet results) {
+        return null;
+    }
+
+    void insert() {}
+    void update() {}
 }
