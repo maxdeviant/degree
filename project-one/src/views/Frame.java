d876475730785053d4d6ada94af31a5f2ecb5244
maxdeviant <mad.mod.max@gmail.com>
2014-02-28 11:59:09 -0500

    ActorController mess
    More planning probably needed.

--- project-one/src/views/Frame.java
+++ project-one/src/views/Frame.java
@@ -1,19 +1,40 @@
 package views;
 
+import controllers.ActorController;
+import mysql.DataHandler;
+
 import javax.swing.*;
 
 public class Frame extends javax.swing.JFrame {
+    private JPanel window;
     private JLabel actorsLabel;
-    private JLabel moviesLabel;
     private JList actors;
+    private DefaultListModel actorModel;
+    private ActorController actorController;
+    private JLabel moviesLabel;
     private JTable movies;
 
     public Frame() {
+        // Form initialization
+        JFrame frame = new JFrame("Main");
+        frame.setContentPane(window);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
         init();
     }
 
     private void init() {
+        DataHandler db = new DataHandler();
 
+        actorModel = new DefaultListModel();
+
+        for (String s : actorController.getActorNames()) {
+            actorModel.addElement(s);
+        }
+
+//        actors.setModel();
     }
 
     public static void main(String[] args) {
