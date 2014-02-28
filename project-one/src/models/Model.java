2f42d3b32e01f1e9097b98781fb134f8524c748c
maxdeviant <mad.mod.max@gmail.com>
2014-02-28 11:16:41 -0500

    Revert a42637c..1ee3711
    This rolls back to commit a42637ca2e62d95ad416c721d87a7c11a97b4373.

--- project-one/src/models/Model.java
+++ project-one/src/models/Model.java
@@ -4,20 +4,10 @@ import java.sql.ResultSet;
 import java.util.HashSet;
 
 public abstract class Model<T> {
-    public int getId() { return 0; }
-
     HashSet<T> load(ResultSet results) {
         return null;
     }
 
     void insert() {}
     void update() {}
-
-    @Override
-    public boolean equals(Object obj) {
-        if (!this.getClass().isInstance(obj)) {
-            return false;
-        }
-        return getId() == ((Model)obj).getId();
-    }
 }
