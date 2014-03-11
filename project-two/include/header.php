37ec7e92a8bc1ee36bb009889d9889ef572ddca2
Marshall Bowers <elliott.codes@gmail.com>
2014-03-11 09:56:46 -0400

    CSS taking shape.

--- project-two/include/header.php
+++ project-two/include/header.php
@@ -8,10 +8,18 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
 
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+
 	<link rel="stylesheet" href="css/layout.css" />
 	<link rel="stylesheet" href="css/style.css" />
 </head>
 <body>
 	<section class="header">
+		<a href="#" id="brand"><?php echo $site; ?></a>
 
+		<ul class="navigation">
+			<li><a href="#">Link</a></li>
+			<li><a href="#">Link</a></li>
+		</ul>
 	</section>
+	<div class="wrapper">	
