b9182c3cf5d0cf7f58ef84487941a67d52b766db
maxdeviant <mad.mod.max@gmail.com>
2014-03-11 10:03:56 -0400

    Work-in-progress footer.

--- project-two/include/footer.php
+++ project-two/include/footer.php
@@ -1,6 +1,10 @@
 	</div>
 	<section class="footer">
-
+		Made with &hearts; by Marshall.
+		<ul class="links">
+			<li><a href="#">Link</a></li>
+			<li><a href="#">Link</a></li>
+		</ul>
 	</section>
 </body>
 </html>
