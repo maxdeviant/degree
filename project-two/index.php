c26fb8ff6dac60feb34f71be36df5e9890f28f10
maxdeviant <mad.mod.max@gmail.com>
2014-03-24 18:50:33 -0400

    Working on display grid.

--- project-two/index.php
+++ project-two/index.php
@@ -1,3 +1,9 @@
+<?php
+	require_once "include/db.php";
+	DB::init();
+
+	$items = R::findall('item', '1 order by name asc');
+?>
 <?php include "include/header.php"; ?>
 <body>
 	<div class="container">
@@ -9,7 +15,20 @@
 			<hr />
 
 			<?php foreach ($items as $item): ?>
-
+				<div class="five columns">
+					<h5><?php echo $item['name']; ?></h5>
+					<hr />
+					<!-- <img src="<?php echo $item['image']; ?>"> -->
+					<div class="item" style="background: url(<?php echo $item['image']; ?>) no-repeat 0 center;"></div>
+					<span>
+						<span class="bold">Price: </span>$<?php echo $item['price']; ?>
+					</span>
+					<br>
+					<span>
+						<span class="bold">Category: </span><?php echo $item['category']; ?>
+					</span>
+					<!-- <span>Description: <?php echo $item['description']; ?></span> -->
+				</div>
 			<?php endforeach; ?>
 		</div>
 	</div>
