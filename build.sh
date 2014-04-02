7913d6cb0c46d1777af85b99101b1ab5b4dfe6fb
Marshall Bowers <elliott.codes@gmail.com>
2014-04-02 18:28:21 -0400

    Standardizing file headers.

--- build.sh
+++ build.sh
@@ -4,14 +4,14 @@ gimli -r -o ./build
 cd ./build
 pdftk \
 	stark.pdf \
-	toc.pdf \
 	purpose.pdf \
 	objectives.pdf \
 	responsibilities.pdf \
 	overview.pdf \
+	planning.pdf \
 	design.pdf \
+	review.pdf \
 	implementation.pdf \
 	qa.pdf \
-	review.pdf \
 	appendix.pdf \
 	cat output team_project.pdf
