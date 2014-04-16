4d5e7a3d3041d2d9e332f70edea8bfe0bc94f80d
Marshall Bowers <elliott.codes@gmail.com>
2014-04-16 15:57:42 -0400

    Add order "processing."

--- project-two/process.php
+++ project-two/process.php
@@ -10,5 +10,21 @@
 
 	DB::init();
 
-	
+	R::setStrictTyping(false);
+
+	$id = $_REQUEST['id'];
+
+	$order = R::load('order', $id);
+	$items = R::find('item_order', 'order_id=?', array($id));
+
+	print_r(array_keys($items));
+
+	foreach (array_keys($items) as $key => $value) {
+		$entry = R::load('item_order', $value);
+		R::trash($entry);
+	}
+
+	R::trash($order);
+
+	header('location: manage_orders.php');
 ?>
