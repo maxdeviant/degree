c92008233648b5726e0a7c226bee0b6957f75bb8
Marshall Bowers <elliott.codes@gmail.com>
2014-04-16 21:11:24 -0400

    Add remove button for items in cart.

--- project-two/cart.php
+++ project-two/cart.php
@@ -8,6 +8,10 @@
 	if (isset($_REQUEST['id'])) {
 		$session->cart[$_REQUEST['id']] = $_REQUEST['quantity'];
 	}
+
+	if (isset($_REQUEST['remove'])) {
+		unset($session->cart[$_REQUEST['remove']]);
+	}
 ?>
 <?php include "include/header.php"; ?>
 <body>
@@ -25,6 +29,7 @@
 					<th>Price</th>
 					<th>Quantity</th>
 					<th>Subtotal</th>
+					<th></th>
 				</tr>
 				<?php foreach (array_keys($session->cart) as $entry):
 					$item = R::findOne('item', 'id=?', array($entry));
@@ -35,6 +40,7 @@
 					<td><?php echo "$ " . number_format($item->price, 2); ?></td>
 					<td><input type="text" id="item-<?php echo $item->id; ?>" value="<?php echo $session->cart[$entry]; ?>" onchange="update(<?php echo $item->id; ?>);" /></td>
 					<td><?php echo "$ " . number_format($session->cart[$entry] * $item->price, 2); ?></td>
+					<td><i class="fa fa-times" onclick="removeEntry(<?php echo $item->id; ?>);"></i></td>
 				</tr>
 				<?php endforeach; ?>
 			</table>
@@ -59,6 +65,10 @@
 			}
 			
 			window.location = "cart.php?id=" + id + "&quantity=" + quantity;
+		}
+
+		function removeEntry(id) {
+			window.location = "cart.php?remove=" + id;
 		}
 	</script>
 </body>
