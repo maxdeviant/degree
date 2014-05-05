e7da83317eb276de7f5bde28a09b75d5ccd1784b
maxdeviant <mad.mod.max@gmail.com>
2014-05-05 11:46:05 -0400

    Add MySQL integration.

--- project-three/fuel/app/views/splash/index.php
+++ project-three/fuel/app/views/splash/index.php
@@ -18,6 +18,13 @@
 						</tr>
 					</thead>
 					<tbody>
+						<?php foreach ($items as $item): ?>
+							<tr>
+								<td><a href="item.php?item_id=<?php echo $item['id']; ?>"><?php echo $item['name']; ?></a></td>
+								<td><?php echo ucfirst($item['category']); ?></td>
+								<td><?php echo "$ " . number_format($item['price'], 2); ?></td>
+							</tr>
+						<?php endforeach; ?>
 					</tbody>
 				</table>
 			</div>
