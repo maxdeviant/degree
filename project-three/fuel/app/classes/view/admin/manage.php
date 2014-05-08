4c6a44e7b889f54d789c22afee9a19301e5d58ba
Marshall Bowers <elliott.codes@gmail.com>
2014-05-08 16:05:08 -0400

    Add admin manage orders view.

---
+++ project-three/fuel/app/classes/view/admin/manage.php
@@ -0,0 +1,8 @@
+<?php
+	class View_Admin_Manage extends ViewModel {
+		public function view() {
+			$this->title = 'Manage Orders &raquo; CSC 417';
+			$this->user = Session::get('user');
+		}
+	}
+?>
