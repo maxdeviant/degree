2b08c498d9f247c59d2cf51bcecca2c2567aa953
Marshall Bowers <elliott.codes@gmail.com>
2014-08-26 19:17:09 -0400

    Working pass-by-reference Java code.

--- homework_0/References.java
+++ homework_0/References.java
@@ -1,5 +1,39 @@
 public class References {
+    static class Int {
+        int value;
+
+        public Int(int value) {
+            this.value = value;
+        }
+
+        public void setInt(int value) {
+            this.value = value;
+        }
+
+        public int getInt() {
+            return value;
+        }
+    }
+
+    private static int square(Int i) {
+        i.setInt(i.getInt() * i.getInt());
+
+        return i.getInt();
+    }
+
+    private static int twice(Int i) {
+        i.setInt(2 * i.getInt());
+
+        return i.getInt();
+    }
+
+    private static int once(Int i) {
+        return i.getInt();
+    }
+
     public static void main(String[] args) {
-        System.out.println("done");
+        Int x = new Int(3);
+        int y = square(x) + twice(x) + once(x);
+        System.out.println(y);
     }
 }
