202cd48b32d7a4f9b132c8ccd7c7d0c1af0b0671
Marshall Bowers <elliott.codes@gmail.com>
2014-09-09 14:49:38 -0400

    Bootstrap homework 2

---
+++ homework_2/problems.js
@@ -0,0 +1,111 @@
+/*
+ *  Problem 1
+ *  Create a variable to store the radius of a circle. Assign the variable some value. Calculate and display the the circle's diameter, circumference, and area.
+ */
+function problemOne() {
+
+}
+
+/*
+ *  Problem 2
+ *  Write a function that takes one argument, a temperature in Celsius, and returns that temperature in Fahrenheit. Print out the resulting temperature. Feel free to hard code the Celsius temperature, or if you wish accept the temperature as input via a text box.
+ */
+function problemTwo() {
+
+}
+
+/*
+ *  Problem 3
+ *  Write a function that takes one argument, the user's birthday year. The function shall return an array whose elements are the possible ages of the user. Only the user's birthday should be hardcoded -- not 2014. Utilize JavaScript functions to return the current calendar year (2014).
+ */
+function problemThree() {
+
+}
+
+/*
+ *  Problem 4
+ *  Write a function that takes one numeric argument and displays that number reversed in an alert box. (i.e. 12345 -> 54321)
+ */
+function problemFour() {
+
+}
+
+/*
+ *  Problem 5
+ *  Write a function that generates three random numbers and outputs the largest number.
+ */
+function problemFive() {
+
+}
+
+/*
+ *  Problem 6
+ *  Write a function that generates all combinations of a given String, and returns the combinations as elements in an array. (i.e. 'bad' -> ['b','ba','bad','a','ad','d'])
+ */
+function problemSix() {
+
+}
+
+/*
+ *  Problem 7
+ *  Write a function that accepts one argument and returns the typeof the argument.
+ */
+function problemSeven() {
+
+}
+
+/*
+ *  Problem 8
+ *  Write a function that takes an array of numbers and finds the second lowest and second greatest numbers. The function should return an object with this information.
+ */
+function problemEight() {
+
+}
+
+/*
+ *  Problem 9
+ *  Write a function that takes two arguments, an array with numeric elements and a number. The function should iterate over the array, and in-place, delete all elements in the array that are smaller than the second argument number.
+ */
+function problemNine() {
+
+}
+
+/*
+ *  Problem 10
+ *  Write a function that takes one numeric argument, n, and outputs the first n happy numbers.
+ */
+function problemTen() {
+
+}
+
+/*
+ *  Problem 11
+ *  Duplicate your above function so that it also incorporates a try/catch block to check for a negative n.
+ */
+function problemEleven() {
+
+}
+
+/*
+ *  Problem 12
+ *  Write a function that accepts a number as input and returns a numeric-like string that has "pluses" + inserted between every two even digits. (i.e. 2647522 -> 2+6+4752+2)
+ */
+function problemTwelve() {
+
+}
+
+/*
+ *  Problem 13
+ *  Write a function that accepts an array argument and returns the most frequent item. (i.e. [5, 'a', 'a', 'a', 3, 1, 'a', 4, 'a', 4, 4] -> "a 5")
+ */
+function problemThirteen() {
+
+}
+
+/*
+ *  Problem 14
+ *  Write a function that prints the properties of an object by iterating over them.
+ */
+function problemFourteen() {
+
+}
