5fd6b6d3a169cd49cbd1de3b6b09084265018c93
Marshall Bowers <elliott.codes@gmail.com>
2014-09-11 15:57:04 -0400

    Add check for all-integer list before executing
    Add docstring.

--- homework_1/homework.lisp
+++ homework_1/homework.lisp
@@ -3,9 +3,10 @@
     ())
 
 (defun add-1-all (L)
-    "Documentation for add-1-all."
+    "Returns the list L of integers with every element increased by one."
     (cond
         ((not (listp L)) nil)
+        ((not (every 'integerp L)) nil)
         ((endp L) nil)
         (t (cons (1+ (first L)) (add-1-all (rest L))))
 ))
