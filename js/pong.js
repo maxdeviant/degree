29a47036aa4e2191e0c00ef7921ae3e4a1bd45b1
Marshall Bowers <elliott.codes@gmail.com>
2014-09-30 16:01:39 -0400

    Add two players with individual movement

--- js/pong.js
+++ js/pong.js
@@ -11,6 +11,8 @@ var STATES = Object.freeze({
 var currentState = STATES.MENU;
 
 var KEYS = Object.freeze({
+    37: 'L_ARR',
+    39: 'R_ARR',
     65: 'A',
     68: 'D'
 });
@@ -31,13 +33,70 @@ window.addEventListener('keyup', function (e) {
     }
 }, false);
 
-var update = function (dt) {
+var Player = function (x, y, CONTROLS) {
+    this.width = 100;
+    this.height = 20;
+    this.x = x || canvas.width / 2 - this.width / 2;
+    this.y = y || 10;
+    this.speed = 10;
 
+    this.step = function (dt) {
+        if (pressed[CONTROLS.LEFT]) {
+            this.vx = -this.speed;
+        } else if (pressed[CONTROLS.RIGHT]) {
+            this.vx = this.speed;
+        } else {
+            this.vx = 0;
+        }
+
+        this.x += this.vx;
+
+        if (this.x < 0) {
+            this.x = 0;
+        } else if (this.x > canvas.width - this.width) { 
+            this.x = canvas.width - this.width;
+        }     
+    };
+
+    this.draw = function () {
+        ctx.fillStyle = '#fff';
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    };
+};
+
+var CONTROLS = Object.freeze({
+    PLAYER_ONE: {
+        LEFT: 'A',
+        RIGHT: 'D'
+    },
+    PLAYER_TWO: {
+        LEFT: 'L_ARR',
+        RIGHT: 'R_ARR'
+    }
+});
+
+var entities = [];
+
+var init = function () {
+    var playerOne = new Player(null, canvas.height - 30, CONTROLS.PLAYER_ONE);
+    var playerTwo = new Player(null, 10, CONTROLS.PLAYER_TWO);
+
+    entities.push(playerOne);
+    entities.push(playerTwo);
+};
+
+var update = function (dt) {
+    for (var i in entities) {
+        entities[i].step(dt);
+        entities[i].draw();
+    }
 };
 
 var loop = function () {
     var now = Date.now();
     var delta = now - then;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     update(delta / 1000);
 
@@ -48,4 +107,5 @@ var then = Date.now();
 
 var requestAnimationFrame = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || window.mozRequestAnimationFrame;
 
+init();
 loop();
