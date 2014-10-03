cd5131e624f77f1eacac19e121e7c00cca966f10
Marshall Bowers <elliott.codes@gmail.com>
2014-10-03 13:25:16 -0400

    Add Gulp

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+
+gulp.task('lint', function () {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('js/*.js', ['lint']);
+});
+
+gulp.task('default', [
+    'lint',
+    'watch'
+]);
