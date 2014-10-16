14b12712fac3466a91f19d6f40b8e0d3c98cde89
Marshall Bowers <elliott.codes@gmail.com>
2014-10-15 23:55:09 -0400

    Update integration

--- project_1/integrate.lisp
+++ project_1/integrate.lisp
@@ -5,6 +5,70 @@
 ;;; integrate.lisp
 
 ;;;=================================================================
+;;;    NAME: integrate
+;;;  ARG(S): 
+;;; RETURNS: 
+
+(defun integrate (F V &optional lower upper)
+    "docstring"
+    (def-integral (indef-integral F V) V lower upper))
+
+;;;=================================================================
+;;;    NAME: indef-integral
+;;;  ARG(S): 
+;;; RETURNS: 
+
+(defun indef-integral (F V)
+    "docstring"
+    (labels (
+        (indef-integral-aux (F V)
+            (cond
+                ((number-p F) (make-product F V))
+                ((negative-p F) (make-negative (integrate (make-negative F) V)))
+                ((variable-p F) (integrate (make-power F 1) V))
+                ((sum-p F) (make-sum
+                    (integrate (sum-first-operand F) V)
+                    (integrate (sum-second-operand F) V)))
+                ((difference-p F) (make-difference
+                    (integrate (difference-first-operand F) V)
+                    (integrate (difference-second-operand F) V)))
+                ((and (power-p F) (not (eq (power-second-operand F) -1))) (make-quotient
+                    (make-power V (make-sum (power-second-operand F) 1))
+                    (make-sum (power-second-operand F) 1)))
+                ((and (power-p F) (eq (power-second-operand F) -1) (make-log (power-first-operand F))))
+                (t nil))))
+        (cond
+            ((mult-negative-p F) (indef-integral-aux (make-reduced-negative F) V))
+            ((variable-p V) (indef-integral-aux F V))
+            (t nil))))
+
+;;;=================================================================
+;;;    NAME: def-integral
+;;;  ARG(S): 
+;;; RETURNS: 
+
+(defun def-integral (F V lower upper)
+    "docstring"
+    (cond
+        ((not (and (number-p lower) (number-p upper))) F)
+        (t (eval (make-difference
+            (my-replace V upper F)
+            (my-replace V lower F))))))
+
+(defun my-replace (e1 e2 L)
+    "docstring"
+    (labels (
+        (my-replace-aux (e1 e2 L)
+            (cond
+                ((endp L) nil)
+                ((equal (first L) e1) (cons e2 (my-replace e1 e2 (rest L))))
+                ((listp (first L)) (cons (my-replace e1 e2 (first L)) (my-replace e1 e2 (rest L))))
+                (t (cons (first L) (my-replace e1 e2 (rest L)))))))
+        (cond
+            ((variable-p L) (first (my-replace e1 e2 (list L))))
+            (t (my-replace-aux e1 e2 L)))))
+
+;;;=================================================================
 ;;; SYMBOLS
 
 (defconstant *variable-symbols* '(U V W X Y Z))
@@ -40,10 +104,6 @@
     (first F))
 
 (defun power-operator (F)
-    "docstring"
-    (first F))
-
-(defun log-operator (F)
     "docstring"
     (first F))
 
@@ -97,7 +157,7 @@
 ;;; PREDICATES
 
 (defun variable-p (F)
-    "Returns T if F is a valid variable symbol"
+    "docstring"
     (member F *variable-symbols*))
 
 (defun number-p (F)
@@ -106,27 +166,60 @@
 
 (defun negative-p (F)
     "docstring"
-    "pass")
+    (cond
+        ((and (number-p F) (< F 0)) t)
+        ((number-p F) nil)
+        ((variable-p F) nil)
+        ((difference-p F) nil)
+        ((and
+            (eq (negative-operator F) *negative-symbol*)
+            (not (eq (negative-operand F) *negative-symbol*))) t)))
+
+(defun mult-negative-p (F)
+    "docstring"
+    (labels (
+        (mult-negative-p-aux (F L)
+            (cond
+                ((endp F) (eq (length L) 1))
+                ((eq (first F) *negative-symbol*) (mult-negative-p-aux (rest F) L))
+                (t (mult-negative-p-aux (rest F) (cons (first F) L))))))
+    (cond
+        ((number-p F) nil)
+        ((variable-p F) nil)
+        ((negative-p F) nil)
+        ((difference-p F) nil)
+        ((not (listp F)) nil)
+        (t (mult-negative-p-aux F '())))))
 
 (defun sum-p (F)
     "docstring"
-    "pass")
+    (cond
+        ((number-p F) nil)
+        ((variable-p F) nil)
+        ((and
+            (eq (sum-operator F) *sum-symbol*)
+            (sum-first-operand F)
+            (sum-second-operand F)) t)))
 
 (defun difference-p (F)
     "docstring"
-    "pass")
-
-(defun product-p (F)
-    "docstring"
-    "pass")
-
-(defun quotient-p (F)
-    "docstring"
-    "pass")
+    (cond
+        ((number-p F) nil)
+        ((variable-p F) nil)
+        ((and
+            (eq (difference-operator F) *difference-symbol*)
+            (not (eq (difference-first-operand F) *difference-symbol*))
+            (difference-second-operand F)) t)))
 
 (defun power-p (F)
     "docstring"
-    "pass")
+    (cond
+        ((number-p F) nil)
+        ((variable-p F) nil)
+        ((and
+            (eq (power-operator F) *power-symbol*)
+            (variable-p (power-first-operand F))
+            (number-p (power-second-operand F))) t)))
 
 ;;;=================================================================
 ;;; CONSTRUCTORS
@@ -137,7 +230,25 @@
 
 (defun make-negative (F)
     "docstring"
-    (list *negative-symbol* F))
+    (labels (
+        (make-negative-aux (F)
+            (cond
+                ((number-p F) (* -1 F))
+                ((negative-p F) (negative-operand F))
+                (t (list *negative-symbol* F)))))
+    (cond
+        ((mult-negative-p F) (make-negative-aux (make-reduced-negative F)))
+        (t (make-negative-aux F)))))
+
+(defun make-reduced-negative (F)
+    (labels (
+        (make-reduced-negative-aux (F)
+            (cond
+                ((eq (mod (length F) 2) 0) (list *negative-symbol* (first (last F))))
+                (t (first (last F))))))
+    (cond
+        ((mult-negative-p F) (make-reduced-negative-aux F))
+        ((negative-p F) F))))
 
 (defun make-sum (F G)
     "docstring"
@@ -155,6 +266,7 @@
         ((eq F 0) (make-negative G))
         ((eq G 0) F)
         ((and (number-p F) (number-p G)) (- F G))
+        ((eq F (make-negative G)) (make-sum F G))
         (t (list *difference-symbol* F G))))
 
 (defun make-product (F G)
@@ -166,9 +278,9 @@
         ((eq G 1) F)
         ((eq F -1) (make-negative G))
         ((eq G -1) (make-negative F))
+        ((and (negative-p F) (negative-p G)) (make-product (make-negative F) (make-negative G)))
         ((and (number-p F) (number-p G)) (* F G))
-        (t (list *product-symbol* F G))
-    ))
+        (t (list *product-symbol* F G))))
 
 (defun make-quotient (F G)
     "docstring"
@@ -176,43 +288,15 @@
         ((eq F 0) 0)
         ((eq G 0) nil)
         ((and (number-p F) (number-p G)) (/ F G))
-        (t (list *quotient-symbol* F G))
-    ))
+        (t (list *quotient-symbol* F G))))
 
 (defun make-power (V N)
     "docstring"
     (cond
         ((and (number-p V) (numberp N)) (expt V N))
-        (t (list *power-symbol* V N))
-    ))
+        (t (list *power-symbol* V N))))
 
-;;;=================================================================
-;;;    NAME: integrate
-;;;  ARG(S): 
-;;; RETURNS: 
-
-(defun integrate (F V &optional lower upper)
-    "docstring"
-    (indef-integral F V)) ; test indefinites only
-    ; (def-integral (indef-integral F V) lower upper))
-
-;;;=================================================================
-;;;    NAME: indef-integral
-;;;  ARG(S): 
-;;; RETURNS: 
-
-(defun indef-integral (F V)
+(defun make-log (V)
     "docstring"
     (cond
-        ((number-p F) (make-product F V))
-        (t "pass")
-    ))
-
-;;;=================================================================
-;;;    NAME: def-integral
-;;;  ARG(S): 
-;;; RETURNS: 
-
-(defun def-integral (F lower upper)
-    "docstring"
-    "pass")
+        ((variable-p V) (list *log-symbol* V))))
