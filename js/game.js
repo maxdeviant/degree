442841c303b9b9156cb00cfb96de9c30f19a050d
Marshall Bowers <elliott.codes@gmail.com>
2014-10-28 14:01:39 -0400

    Add base Quintus code

--- js/game.js
+++ js/game.js
@@ -1,1 +1,13 @@
 'use strict';
+
+var Q = Quintus()
+    .include('Sprites, Scenes, Input, 2D')
+    .setup();
+
+Q.scene('menu', function (stage) {
+
+});
+
+Q.clearColor = '#000';
+
+Q.stageScene('menu', 0);
