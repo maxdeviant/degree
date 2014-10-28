914569d7a7265fa3bbe7efbb0347e5e8e2544617
Marshall Bowers <elliott.codes@gmail.com>
2014-10-28 16:02:30 -0400

    Improve line spacing

--- homework_4/blockbreak.js
+++ homework_4/blockbreak.js
@@ -21,19 +21,21 @@ $(function () {
                 speed: 200,
                 x: 0,
             });
+
             this.p.x = Q.width / 2 - this.p.w / 2;
             this.p.y = Q.height - this.p.h;
+
             if (Q.input.keypad.size) {
                 this.p.y -= Q.input.keypad.size + this.p.h;
             }
         },
-
         step: function (dt) {
             if (Q.inputs.left) {
                 this.p.x -= dt * this.p.speed;
             } else if (Q.inputs.right) {
                 this.p.x += dt * this.p.speed;
             }
+
             if (this.p.x < this.p.w / 2) {
                 this.p.x = this.p.w / 2;
             } else if (this.p.x > Q.width - this.p.w / 2) {
@@ -50,15 +52,17 @@ $(function () {
                 dx: 1,
                 dy: -1,
             });
+
             this.p.y = Q.height / 2 - this.p.h;
             this.p.x = Q.width / 2 + this.p.w / 2;
 
             this.on('hit', this, 'collision');
+
             this.on('step', function (dt) {
                 var p = this.p;
 
                 Q.stage().collide(this);
-                
+
                 p.x += p.dx * p.speed * dt;
                 p.y += p.dy * p.speed * dt;
 
@@ -78,7 +82,6 @@ $(function () {
                 }
             });
         },
-
         collision: function (col) {
             if (col.obj.isA('Paddle')) {
                 this.p.dy = -1;
@@ -95,6 +98,7 @@ $(function () {
             this._super(_(props).extend({
                 sheet: 'block'
             }));
+            
             this.on('collision', function (ball) {
                 this.destroy();
                 ball.p.dy *= -1;
@@ -106,40 +110,48 @@ $(function () {
     Q.load(['blockbreak.png', 'blockbreak.json'], function () {
         Q.load(['blockbreak.png'], function () {
             Q.compileSheets('blockbreak.png', 'blockbreak.json');
+
             Q.sheet('ball', 'blockbreak.png', {
                 tilew: 20,
                 tileh: 20,
                 sy: 0,
                 sx: 0
             });
+
             Q.sheet('block', 'blockbreak.png', {
                 tilew: 40,
                 tileh: 20,
                 sy: 20,
                 sx: 0
             });
+
             Q.sheet('paddle', 'blockbreak.png', {
                 tilew: 60,
                 tileh: 20,
                 sy: 40,
                 sx: 0
             });
+
             Q.scene('game', new Q.Scene(function (stage) {
                 stage.insert(new Q.Paddle());
                 stage.insert(new Q.Ball());
 
                 var blockCount = 0;
+
                 for (var x = 0; x < 6; x++) {
                     for (var y = 0; y < 5; y++) {
                         stage.insert(new Q.Block({
                             x: x * 50 + 35,
                             y: y * 30 + 15
                         }));
+
                         blockCount++;
                     }
                 }
+
                 stage.on('removeBlock', function () {
                     blockCount--;
+
                     if (blockCount === 0) {
                         Q.stageScene('game');
                     }
