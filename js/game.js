c28dbf1a4bf17db9d97467c13010575a487b7a6b
Marshall Bowers <elliott.codes@gmail.com>
2014-10-28 14:42:26 -0400

    Add menu elements

--- js/game.js
+++ js/game.js
@@ -1,9 +1,28 @@
 'use strict';
 
-var Q = new Quintus().include('Sprites, Scenes, Input, 2D').setup();
+var Q = new Quintus().include('Sprites, Scenes, Input, 2D, UI').setup();
 
 Q.scene('menu', function (stage) {
+    var container = stage.insert(new Q.UI.Container({
+        x: Q.width / 2,
+        y: Q.height / 4,
+        fill: '#000'
+    }));
 
+    stage.insert(new Q.UI.Text({ 
+        label: 'Project Two',
+        color: '#fff',
+        x: 0,
+        y: 0
+    }), container);
+
+    stage.insert(new Q.UI.Button({ 
+        label: 'Start',
+        fill: '#fff',
+        x: 0,
+        y: 70,
+        w: 100
+    }), container);
 });
 
 Q.clearColor = '#000';
