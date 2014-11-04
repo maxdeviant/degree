2d39d7663716a67b4dbd7f6ed2cf778b325f996f
Marshall Bowers <elliott.codes@gmail.com>
2014-11-04 11:17:53 -0500

    Fix click event on buttons

--- js/game.js
+++ js/game.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var Q = new Quintus().include('Sprites, Scenes, Input, 2D, UI').setup();
+var Q = new Quintus().include('Sprites, Scenes, Input, Touch, 2D, UI').setup().touch();
 
 Q.scene('menu', function (stage) {
     var container = stage.insert(new Q.UI.Container({
@@ -9,20 +9,26 @@ Q.scene('menu', function (stage) {
         fill: '#000'
     }));
 
-    stage.insert(new Q.UI.Text({
+    container.insert(new Q.UI.Text({
         label: 'Project Two',
         color: '#fff',
         x: 0,
         y: 0
-    }), container);
+    }));
 
-    stage.insert(new Q.UI.Button({
+    container.insert(new Q.UI.Button({
         label: 'Start',
         fill: '#fff',
         x: 0,
         y: 70,
         w: 100
-    }), container);
+    }, function () {
+        Q.stageScene('game');
+    }));
+});
+
+Q.scene('game', function (stage) {
+
 });
 
 Q.clearColor = '#000';
