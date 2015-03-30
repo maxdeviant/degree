b3f272a21218279298046b8348db0990e97b3c09
Marshall Bowers <elliott.codes@gmail.com>
2015-03-30 12:12:03 -0400

    Calculate e and d

--- assignment-1/src/RSA.java
+++ assignment-1/src/RSA.java
@@ -12,17 +12,43 @@ public class RSA {
     private BigInteger p;
     private BigInteger q;
     private BigInteger n;
+    private BigInteger phi;
+    private BigInteger e;
+    private BigInteger d;
 
     public RSA() {
-        p = BigInteger.probablePrime(2048, new Random());
-        q = BigInteger.probablePrime(2048, new Random());
+        p = BigInteger.probablePrime(256, new Random());
+        q = BigInteger.probablePrime(256, new Random());
 
-        System.out.println(p);
-        System.out.println(q);
+//        p = new BigInteger("61");
+//        q = new BigInteger("53");
+
+//        System.out.println(p);
+//        System.out.println(q);
 
         n = p.multiply(q);
 
-        System.out.println(n);
+//        System.out.println(n);
+
+        phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+
+        System.out.println(phi);
+
+        e = BigInteger.ONE;
+
+        while (e.intValue() < phi.intValue()) {
+            e = e.nextProbablePrime();
+
+            if (phi.gcd(e).intValue() == 1) {
+                break;
+            }
+        }
+
+        System.out.println(e);
+
+        d = e.modInverse(phi);
+
+        System.out.println(d);
     }
 
 }
