64ac04960c1a37836927124786299f37aff7eea5
Marshall Bowers <elliott.codes@gmail.com>
2015-04-14 15:04:15 -0400

    Improve output more

--- assignment-1/src/Test.java
+++ assignment-1/src/Test.java
@@ -9,12 +9,12 @@ public class Test {
     public static void main(String[] argv) {
         RSA rsa = new RSA(1024);
 
-        String message = "Hello, World!";
+        String message = "The quick brown fox jumps over the lazy dog";
 
-        System.out.println("Public Key: " + rsa.getPublicKey());
-        System.out.println("Private Key: " + rsa.getPrivateKey());
+        System.out.println("Public Key  <e, n>: " + rsa.getPublicKey());
+        System.out.println("Private Key <d, n>: " + rsa.getPrivateKey());
 
-        System.out.println("Original Message:  " + message);
+        System.out.println("Original Message:  \"" + message + "\"");
 
         String encrypted = rsa.encrypt(message);
 
@@ -22,11 +22,9 @@ public class Test {
 
         String decrypted = rsa.decrypt(encrypted);
 
-        System.out.println("Decrypted Message: " + decrypted);
+        System.out.println("Decrypted Message: \"" + decrypted + "\"");
 
-        System.out.println(decrypted);
-
-        System.out.println("Verified: " + rsa.isVerified(message));
+        System.out.println("Verified: " + (rsa.isVerified(message) ? "Yes" : "No") + ".");
     }
 
 }
