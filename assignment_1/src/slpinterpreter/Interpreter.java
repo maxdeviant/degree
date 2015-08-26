082b07233b716e1273fca3571457e8ad7b613eb2
Marshall Bowers <elliott.codes@gmail.com>
2015-08-26 18:49:05 -0400

    Add Table class for interpreter

--- assignment_1/src/slpinterpreter/Interpreter.java
+++ assignment_1/src/slpinterpreter/Interpreter.java
@@ -7,6 +7,18 @@ import slpinterpreter.grammar.Stm;
  */
 public class Interpreter {
 
+    private class Table {
+        String id;
+        int value;
+        Table tail;
+
+        public Table(String id, int value, Table table) {
+            this.id = id;
+            this.value = value;
+            this.tail = table;
+        }
+    }
+
     public static void interp(Stm stm) {
 
 
