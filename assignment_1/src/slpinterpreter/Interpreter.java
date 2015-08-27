67c1d8a9a93d11879b78001a3495d8b8676a31b2
Marshall Bowers <elliott.codes@gmail.com>
2015-08-27 14:50:56 -0400

    Move arithmetic operation evaluation to own function

--- assignment_1/src/slpinterpreter/Interpreter.java
+++ assignment_1/src/slpinterpreter/Interpreter.java
@@ -21,21 +21,6 @@ public class Interpreter {
 
     }
 
-    /**
-     * Returns the value for the specified ID in the specified table.
-     *
-     * @param table The table to lookup the value in.
-     * @param id    The ID to lookup the value for.
-     * @return The value associated with the specified ID.
-     */
-    private static int lookup(Table table, String id) {
-        if (table.id.equals(id)) {
-            return table.value;
-        }
-
-        return lookup(table.tail, id);
-    }
-
     private static class IntAndTable {
 
         int i;
@@ -57,6 +42,13 @@ public class Interpreter {
         interpStm(stm, null);
     }
 
+    /**
+     * Interprets a statement and any child statements or expressions.
+     *
+     * @param stm   The statement to interpret.
+     * @param table The identifier table to use.
+     * @return The identifier table after interpreting the statement.
+     */
     private static Table interpStm(Stm stm, Table table) {
         if (stm instanceof CompoundStm) {
             CompoundStm compoundStm = (CompoundStm) stm;
@@ -71,9 +63,18 @@ public class Interpreter {
             return new Table(assignStm.id, intAndTable.i, intAndTable.table);
         }
 
-        return print(((PrintStm) stm).exps, table);
+        PrintStm printStm = (PrintStm) stm;
+
+        return print(printStm.exps, table);
     }
 
+    /**
+     * Interprets an expression and any child expressions.
+     *
+     * @param exp   The expression to interpret.
+     * @param table The identifier table to use.
+     * @return The identifier table.
+     */
     private static IntAndTable interpExp(Exp exp, Table table) {
         if (exp instanceof IdExp) {
             IdExp idExp = (IdExp) exp;
@@ -98,26 +99,18 @@ public class Interpreter {
         IntAndTable lhsIntAndTable = interpExp(opExp.left, table);
         IntAndTable rhsIntAndTable = interpExp(opExp.right, lhsIntAndTable.table);
 
-        int result = 0;
+        int opResult = evaluateOperation(opExp.oper, lhsIntAndTable.i, rhsIntAndTable.i);
 
-        switch (opExp.oper) {
-            case OpExp.Plus:
-                result = lhsIntAndTable.i + rhsIntAndTable.i;
-                break;
-            case OpExp.Minus:
-                result = lhsIntAndTable.i - rhsIntAndTable.i;
-                break;
-            case OpExp.Times:
-                result = lhsIntAndTable.i * rhsIntAndTable.i;
-                break;
-            case OpExp.Div:
-                result = lhsIntAndTable.i / rhsIntAndTable.i;
-                break;
-        }
-
-        return new IntAndTable(result, rhsIntAndTable.table);
+        return new IntAndTable(opResult, rhsIntAndTable.table);
     }
 
+    /**
+     * Prints an expression list.
+     *
+     * @param expList The expression list to print.
+     * @param table   The identifier table to use.
+     * @return The identifier table.
+     */
     private static Table print(ExpList expList, Table table) {
         if (expList instanceof PairExpList) {
             PairExpList pairExpList = (PairExpList) expList;
@@ -130,24 +123,87 @@ public class Interpreter {
         return print(lastExpList, table);
     }
 
+    /**
+     * Prints a pair expression list.
+     *
+     * @param pairExpList The pair expression list to print.
+     * @param table       The identifier table to use.
+     * @return The identifier table.
+     */
     private static Table print(PairExpList pairExpList, Table table) {
-        return print(pairExpList.tail, doPrint(pairExpList.head, table, false));
+        return print(pairExpList.tail, print(pairExpList.head, table, false));
     }
 
+    /**
+     * Prints the last element in an expression list.
+     *
+     * @param lastExpList The last element in an expression list to print.
+     * @param table       The identifier table to use.
+     * @return The identifier table.
+     */
     private static Table print(LastExpList lastExpList, Table table) {
-        return doPrint(lastExpList.head, table, true);
+        return print(lastExpList.head, table, true);
     }
 
-    private static Table doPrint(Exp exp, Table table, boolean newline) {
+    /**
+     * Prints an expression.
+     *
+     * @param exp           The expression to print.
+     * @param table         The identifier table to use.
+     * @param insertNewline Flag indicating whether or not a newline should be inserted after the printed result of the interpreted expression.
+     * @return The identifier table after interpreting the expression.
+     */
+    private static Table print(Exp exp, Table table, boolean insertNewline) {
         IntAndTable intAndTable = interpExp(exp, table);
 
-        if (newline) {
-            System.out.println(intAndTable.i);
+        System.out.print(intAndTable.i);
+
+        if (insertNewline) {
+            System.out.println();
         } else {
-            System.out.print(intAndTable.i + " ");
+            System.out.print(" ");
         }
 
         return intAndTable.table;
+    }
+
+    /**
+     * Returns the value for the specified ID in the specified table.
+     *
+     * @param table The table to lookup the value in.
+     * @param id    The ID to lookup the value for.
+     * @return The value associated with the specified ID.
+     */
+    private static int lookup(Table table, String id) {
+        if (table.id.equals(id)) {
+            return table.value;
+        }
+
+        return lookup(table.tail, id);
+    }
+
+    /**
+     * Evaluates an arithmetic operation between two integer values.
+     *
+     * @param operation The integer value representing an arithmetic operation to perform.
+     * @param lhs       The integer on the left-hand side of the operator.
+     * @param rhs       The interger on the right-hand side of the operator.
+     * @return The result of the evaluated operation.
+     * @throws IllegalArgumentException Operation must be one of the ones defined in {@link slpinterpreter.grammar.OpExp}.
+     */
+    private static int evaluateOperation(int operation, int lhs, int rhs) throws IllegalArgumentException {
+        switch (operation) {
+            case OpExp.Plus:
+                return lhs + rhs;
+            case OpExp.Minus:
+                return lhs - rhs;
+            case OpExp.Times:
+                return lhs * rhs;
+            case OpExp.Div:
+                return lhs / rhs;
+        }
+
+        throw new IllegalArgumentException("Operation not recognized.");
     }
 
 }
