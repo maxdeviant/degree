9f4c9486a56c1e126a3e9bbc35460daade7490c2
Marshall Bowers <elliott.codes@gmail.com>
2015-10-12 17:22:54 -0400

    Finish problem 2

---
+++ assignment_2/problem_2/src/PrintB.java
@@ -0,0 +1,11 @@
+/**
+ * @author Marshall Bowers
+ */
+public class PrintB implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.print("b");
+    }
+
+}
---
+++ assignment_2/problem_2/src/Problem2.java
@@ -0,0 +1,26 @@
+/**
+ * @author Marshall Bowers
+ */
+public class Problem2 {
+
+   public static void main(String[] args) throws InterruptedException {
+       Thread threadA = new Thread(new PrintA());
+       Thread threadB = new Thread(new PrintB());
+       Thread threadC = new Thread(new PrintC());
+
+       Thread[] threads = {threadA, threadB, threadC};
+
+       int runningThread = 0;
+
+
+       while (true) {
+           threads[runningThread % threads.length].run();
+           threads[runningThread % threads.length].interrupt();
+
+           if (++runningThread == 3000) {
+               break;
+           }
+       }
+   }
+
+}
