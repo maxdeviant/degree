8d1efcdc90c365b69cdf176adfdb082c42db310c
Marshall Bowers <elliott.codes@gmail.com>
2015-10-26 17:52:48 -0400

    Add assignment 3 base files

---
+++ assignment_3/include/error.h
@@ -0,0 +1,11 @@
+/**
+ * Marshall Bowers
+ * Dr. Bin Lu
+ * CSC331-80
+ * 16 November 2015
+ * error.h
+ */
+
+#pragma once
+
+void error_and_die(const char *message);
---
+++ assignment_3/src/main.c
@@ -0,0 +1,15 @@
+/**
+ * Marshall Bowers
+ * Dr. Bin Lu
+ * CSC331-80
+ * 16 November 2015
+ * main.c
+ */
+
+#include "error.h"
+
+int main(int argc, const char *argv[]) {
+
+
+    return 0;
+}
