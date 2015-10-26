8272ed8215e3d74631ed47cce95f335bd1835c8e
Marshall Bowers <elliott.codes@gmail.com>
2015-10-26 17:57:45 -0400

    Get the input values

--- assignment_3/src/main.c
+++ assignment_3/src/main.c
@@ -6,10 +6,21 @@
  * main.c
  */
 
+#include <stdlib.h>
+#include <stdio.h>
+
 #include "error.h"
 
 int main(int argc, const char *argv[]) {
+    if (argc < 3) {
+        error_and_die("Missing required input.");
+    }
 
+    int sleep_duration = atoi(argv[1]);
+
+    int producer_count = atoi(argv[2]);
+
+    int consumer_count = atoi(argv[3]);
 
     return 0;
 }
