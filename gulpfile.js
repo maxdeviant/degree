586f9fafd4293854172d0aa6669b2b7ff1d0cac9
Marshall Bowers <elliott.codes@gmail.com>
2016-04-04 18:34:43 -0400

    Add JSON data

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,38 @@
 'use strict';
 
 const gulp = require('gulp');
+const path = require('path');
+const data = require('gulp-data');
 const nano = require('gulp-cssnano');
 const concat = require('gulp-concat');
 const postcss = require('gulp-postcss');
+const markdown = require('gulp-markdown');
+const nunjucks = require('gulp-nunjucks');
 const sourcemaps = require('gulp-sourcemaps');
 
 const globs = {
+    pages: [
+        'src/site/**/*.html',
+        '!src/site/_layouts/*.html'
+    ],
+    markdown: '',
     css: 'src/css/**/*.css'
 };
+
+gulp.task('html', () => {
+    return gulp.src(globs.pages)
+        .pipe(data((file) => {
+            let dataPath = path.join(path.dirname(file.path), path.basename(file.path).replace(/\.[^/.]+$/, '') + '.json');
+
+            return require(dataPath);
+        }))
+        .pipe(nunjucks.compile())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('markdown', () => {
+
+});
 
 gulp.task('css', () => {
     return gulp.src(globs.css)
@@ -27,6 +51,7 @@ gulp.task('css:watch', () => {
 });
 
 gulp.task('default', [
+    'html',
     'css',
     'css:watch'
 ]);
--- src/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const path = require('path');
