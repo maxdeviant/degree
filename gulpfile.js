2c71c500898c5827f48db0c7a13d57888f067bfc
Marshall Bowers <elliott.codes@gmail.com>
2016-04-04 21:40:37 -0400

    Set up browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,8 @@ const postcss = require('gulp-postcss');
 const markdown = require('gulp-markdown');
 const nunjucks = require('gulp-nunjucks');
 const sourcemaps = require('gulp-sourcemaps');
+const loadJsonFile = require('load-json-file');
+const browserSync = require('browser-sync').create();
 
 const globs = {
     pages: [
@@ -26,12 +28,22 @@ gulp.task('html', () => {
             let baseData = require('./src/site.json');
             let dataPath = path.join(path.dirname(file.path), path.basename(file.path).replace(/\.[^/.]+$/, '') + '.json');
 
-            let pageData = extend(true, baseData, require(dataPath));
+            let pageData = extend(true, baseData, loadJsonFile.sync(dataPath));
 
             return pageData;
         }))
         .pipe(nunjucks.compile())
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
+});
+
+gulp.task('html:watch', () => {
+    return gulp.watch([
+        'src/**/*.json',
+        'src/site/**/*.html'
+    ], ['html']);
 });
 
 gulp.task('markdown', () => {
@@ -47,15 +59,28 @@ gulp.task('css', () => {
         ]))
         .pipe(concat('style.min.css'))
         .pipe(nano())
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
 gulp.task('css:watch', () => {
     gulp.watch(globs.css, ['css']);
 });
 
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist/'
+        }
+    });
+});
+
 gulp.task('default', [
     'html',
+    'html:watch',
     'css',
-    'css:watch'
+    'css:watch',
+    'browser-sync'
 ]);
